3f191ae62008a0c3c387509654dcdcad4adbb188
Ashik Jishy <ashikjishy001@gmail.com>
2026-01-06 09:59:48 +0530

    Added book, author  list view and detail view. Added class views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
 
 
 from .models import Book, Author, BookInstance, Genre
@@ -26,3 +26,17 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, "index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model=Book
+
+class AuthorListview(generic.ListView):
+    model=Author
+
+class AuthorDetailView(generic.DetailView):
+    model=Author
