0ce8b014531fe776627220126f34b2fad1db182e
Ashik Jishy <ashikjishy001@gmail.com>
2026-01-06 14:40:48 +0530

    Implemented a session technique to check the number of visits

--- catalog/views.py
+++ catalog/views.py
@@ -12,16 +12,19 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()   
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
 
-    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+
+    num_visits = request.session.get("num_visits", 0) + 1
+    request.session["num_visits"] = num_visits
 
     context = {
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -32,11 +35,14 @@ class BookListView(generic.ListView):
     model = Book
     paginate_by = 5
 
+
 class BookDetailView(generic.DetailView):
-    model=Book
+    model = Book
+
 
 class AuthorListview(generic.ListView):
-    model=Author
+    model = Author
+
 
 class AuthorDetailView(generic.DetailView):
-    model=Author
+    model = Author
