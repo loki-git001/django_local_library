e7cb1ad8c5963efa54433b41308fffaa0de47c1a
Ashik Jishy <ashikjishy001@gmail.com>
2026-01-18 18:14:59 +0530

    Created author serializer, api urls author_list and author_detail

--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .serializers import AuthorSerializer
 
 from .models import Book, Author, BookInstance, Genre, Language
 
@@ -265,3 +269,52 @@ class BookInstanceDelete(PermissionRequiredMixin, DeleteView):
     model = BookInstance
     success_url = reverse_lazy('bookinstances')
     permission_required = 'catalog.delete_bookinstance'
+
+
+@csrf_exempt
+def author_list(request):
+    """
+    List all authors, or create a new author.
+    """
+    if request.method == "GET":
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = AuthorSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    return HttpResponse(status=400)
+
+@csrf_exempt
+def author_detail(request, pk):
+    """
+    Retrieve, update or delete a code author.
+    """
+    try:
+        author = Author.objects.get(pk=pk)
+    except Author.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = AuthorSerializer(author)
+        return JsonResponse(serializer.data)
+
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = AuthorSerializer(author, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        author.delete()
+        return HttpResponse(status=204)
+
+    return HttpResponse(status=400)
